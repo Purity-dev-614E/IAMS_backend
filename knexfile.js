89d2466a915f0d71f8f617bed77eca2dfed828ff
Purity <puritysang180@gmail.com>
2026-04-27 12:14:58 +0300

    Remove hardcoded PostgreSQL credentials from knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      connectionString: process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_I0EfYOwHTys6@ep-weathered-snow-ai1nr05y-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+      connectionString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false }
     },
     migrations: {
